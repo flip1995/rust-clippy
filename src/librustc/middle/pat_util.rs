rust-lang/rust

rustc: avoid `use`-ing `syntax::ast::*`.

src/librustc/middle/pat_util.rs
@@ -13,16 +13,16 @@ use middle::resolve;
 use middle::ty;
 
 use std::collections::HashMap;
-use syntax::ast::*;
+use syntax::ast;
 use syntax::ast_util::{walk_pat};
 use syntax::codemap::{Span, DUMMY_SP};
 use syntax::owned_slice::OwnedSlice;
 
-pub type PatIdMap = HashMap<Ident, NodeId>;
+pub type PatIdMap = HashMap<ast::Ident, ast::NodeId>;
 
 // This is used because same-named variables in alternative patterns need to
 // use the NodeId of their namesake in the first pattern.
-pub fn pat_id_map(dm: &resolve::DefMap, pat: &Pat) -> PatIdMap {
+pub fn pat_id_map(dm: &resolve::DefMap, pat: &ast::Pat) -> PatIdMap {
     let mut map = HashMap::new();
     pat_bindings(dm, pat, |_bm, p_id, _s, path1| {
       map.insert(path1.node, p_id);
@@ -30,9 +30,11 @@ pub fn pat_id_map(dm: &resolve::DefMap, pat: &Pat) -> PatIdMap {
     map
 }
 
-pub fn pat_is_variant_or_struct(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_variant_or_struct(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatEnum(_, _) | PatIdent(_, _, None) | PatStruct(..) => {
+        ast::PatEnum(_, _) |
+        ast::PatIdent(_, _, None) |
+        ast::PatStruct(..) => {
             match dm.borrow().find(&pat.id) {
                 Some(&DefVariant(..)) | Some(&DefStruct(..)) => true,
                 _ => false
@@ -42,9 +44,9 @@ pub fn pat_is_variant_or_struct(dm: &resolve::DefMap, pat: &Pat) -> bool {
     }
 }
 
-pub fn pat_is_const(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_const(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatIdent(_, _, None) | PatEnum(..) => {
+        ast::PatIdent(_, _, None) | ast::PatEnum(..) => {
             match dm.borrow().find(&pat.id) {
                 Some(&DefStatic(_, false)) => true,
                 _ => false
@@ -54,9 +56,9 @@ pub fn pat_is_const(dm: &resolve::DefMap, pat: &Pat) -> bool {
     }
 }
 
-pub fn pat_is_binding(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_binding(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatIdent(..) => {
+        ast::PatIdent(..) => {
             !pat_is_variant_or_struct(dm, pat) &&
             !pat_is_const(dm, pat)
         }
@@ -64,10 +66,10 @@ pub fn pat_is_binding(dm: &resolve::DefMap, pat: &Pat) -> bool {
     }
 }
 
-pub fn pat_is_binding_or_wild(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_binding_or_wild(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatIdent(..) => pat_is_binding(dm, pat),
-        PatWild(_) => true,
+        ast::PatIdent(..) => pat_is_binding(dm, pat),
+        ast::PatWild(_) => true,
         _ => false
     }
 }
@@ -75,11 +77,11 @@ pub fn pat_is_binding_or_wild(dm: &resolve::DefMap, pat: &Pat) -> bool {
 /// Call `it` on every "binding" in a pattern, e.g., on `a` in
 /// `match foo() { Some(a) => (), None => () }`
 pub fn pat_bindings(dm: &resolve::DefMap,
-                    pat: &Pat,
-                    it: |BindingMode, NodeId, Span, &SpannedIdent|) {
+                    pat: &ast::Pat,
+                    it: |ast::BindingMode, ast::NodeId, Span, &ast::SpannedIdent|) {
     walk_pat(pat, |p| {
         match p.node {
-          PatIdent(binding_mode, ref pth, _) if pat_is_binding(dm, p) => {
+          ast::PatIdent(binding_mode, ref pth, _) if pat_is_binding(dm, p) => {
             it(binding_mode, p.id, p.span, pth);
           }
           _ => {}
@@ -90,7 +92,7 @@ pub fn pat_bindings(dm: &resolve::DefMap,
 
 /// Checks if the pattern contains any patterns that bind something to
 /// an ident, e.g. `foo`, or `Foo(foo)` or `foo @ Bar(..)`.
-pub fn pat_contains_bindings(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_contains_bindings(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     let mut contains_bindings = false;
     walk_pat(pat, |p| {
         if pat_is_binding(dm, p) {
@@ -103,9 +105,9 @@ pub fn pat_contains_bindings(dm: &resolve::DefMap, pat: &Pat) -> bool {
     contains_bindings
 }
 
-pub fn simple_identifier<'a>(pat: &'a Pat) -> Option<&'a Ident> {
+pub fn simple_identifier<'a>(pat: &'a ast::Pat) -> Option<&'a ast::Ident> {
     match pat.node {
-        PatIdent(BindByValue(_), ref path1, None) => {
+        ast::PatIdent(ast::BindByValue(_), ref path1, None) => {
             Some(&path1.node)
         }
         _ => {
@@ -114,11 +116,11 @@ pub fn simple_identifier<'a>(pat: &'a Pat) -> Option<&'a Ident> {
     }
 }
 
-pub fn def_to_path(tcx: &ty::ctxt, id: DefId) -> Path {
-    ty::with_path(tcx, id, |mut path| Path {
+pub fn def_to_path(tcx: &ty::ctxt, id: ast::DefId) -> ast::Path {
+    ty::with_path(tcx, id, |mut path| ast::Path {
         global: false,
-        segments: path.last().map(|elem| PathSegment {
-            identifier: Ident::new(elem.name()),
+        segments: path.last().map(|elem| ast::PathSegment {
+            identifier: ast::Ident::new(elem.name()),
             lifetimes: vec!(),
             types: OwnedSlice::empty()
         }).into_iter().collect(),

flip1995/rust-clippy

src/librustc/middle/pat_util.rs
@@ -13,15 +13,15 @@ use middle::resolve;
 use middle::ty;
 use util::nodemap::FnvHashMap;
 
-use syntax::ast::*;
+use syntax::ast;
 use syntax::ast_util::{walk_pat};
 use syntax::codemap::{Span, DUMMY_SP};
 
-pub type PatIdMap = FnvHashMap<Ident, NodeId>;
+pub type PatIdMap = FnvHashMap<ast::Ident, ast::NodeId>;
 
 // This is used because same-named variables in alternative patterns need to
 // use the NodeId of their namesake in the first pattern.
-pub fn pat_id_map(dm: &resolve::DefMap, pat: &Pat) -> PatIdMap {
+pub fn pat_id_map(dm: &resolve::DefMap, pat: &ast::Pat) -> PatIdMap {
     let mut map = FnvHashMap::new();
     pat_bindings(dm, pat, |_bm, p_id, _s, path1| {
         map.insert(path1.node, p_id);
@@ -29,23 +29,27 @@ pub fn pat_id_map(dm: &resolve::DefMap, pat: &Pat) -> PatIdMap {
     map
 }
 
-pub fn pat_is_refutable(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_refutable(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatLit(_) | PatRange(_, _) => true,
-        PatEnum(_, _) | PatIdent(_, _, None) | PatStruct(..) => {
+        ast::PatLit(_) | ast::PatRange(_, _) => true,
+        ast::PatEnum(_, _) |
+        ast::PatIdent(_, _, None) |
+        ast::PatStruct(..) => {
             match dm.borrow().get(&pat.id) {
                 Some(&DefVariant(..)) => true,
                 _ => false
             }
         }
-        PatVec(_, _, _) => true,
+        ast::PatVec(_, _, _) => true,
         _ => false
     }
 }
 
-pub fn pat_is_variant_or_struct(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_variant_or_struct(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatEnum(_, _) | PatIdent(_, _, None) | PatStruct(..) => {
+        ast::PatEnum(_, _) |
+        ast::PatIdent(_, _, None) |
+        ast::PatStruct(..) => {
             match dm.borrow().get(&pat.id) {
                 Some(&DefVariant(..)) | Some(&DefStruct(..)) => true,
                 _ => false
@@ -55,9 +59,9 @@ pub fn pat_is_variant_or_struct(dm: &resolve::DefMap, pat: &Pat) -> bool {
     }
 }
 
-pub fn pat_is_const(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_const(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatIdent(_, _, None) | PatEnum(..) => {
+        ast::PatIdent(_, _, None) | ast::PatEnum(..) => {
             match dm.borrow().get(&pat.id) {
                 Some(&DefConst(..)) => true,
                 _ => false
@@ -67,9 +71,9 @@ pub fn pat_is_const(dm: &resolve::DefMap, pat: &Pat) -> bool {
     }
 }
 
-pub fn pat_is_binding(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_binding(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatIdent(..) => {
+        ast::PatIdent(..) => {
             !pat_is_variant_or_struct(dm, pat) &&
             !pat_is_const(dm, pat)
         }
@@ -77,10 +81,10 @@ pub fn pat_is_binding(dm: &resolve::DefMap, pat: &Pat) -> bool {
     }
 }
 
-pub fn pat_is_binding_or_wild(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_is_binding_or_wild(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     match pat.node {
-        PatIdent(..) => pat_is_binding(dm, pat),
-        PatWild(_) => true,
+        ast::PatIdent(..) => pat_is_binding(dm, pat),
+        ast::PatWild(_) => true,
         _ => false
     }
 }
@@ -88,11 +92,11 @@ pub fn pat_is_binding_or_wild(dm: &resolve::DefMap, pat: &Pat) -> bool {
 /// Call `it` on every "binding" in a pattern, e.g., on `a` in
 /// `match foo() { Some(a) => (), None => () }`
 pub fn pat_bindings(dm: &resolve::DefMap,
-                    pat: &Pat,
-                    it: |BindingMode, NodeId, Span, &SpannedIdent|) {
+                    pat: &ast::Pat,
+                    it: |ast::BindingMode, ast::NodeId, Span, &ast::SpannedIdent|) {
     walk_pat(pat, |p| {
         match p.node {
-          PatIdent(binding_mode, ref pth, _) if pat_is_binding(dm, p) => {
+          ast::PatIdent(binding_mode, ref pth, _) if pat_is_binding(dm, p) => {
             it(binding_mode, p.id, p.span, pth);
           }
           _ => {}
@@ -103,7 +107,7 @@ pub fn pat_bindings(dm: &resolve::DefMap,
 
 /// Checks if the pattern contains any patterns that bind something to
 /// an ident, e.g. `foo`, or `Foo(foo)` or `foo @ Bar(..)`.
-pub fn pat_contains_bindings(dm: &resolve::DefMap, pat: &Pat) -> bool {
+pub fn pat_contains_bindings(dm: &resolve::DefMap, pat: &ast::Pat) -> bool {
     let mut contains_bindings = false;
     walk_pat(pat, |p| {
         if pat_is_binding(dm, p) {
@@ -116,9 +120,9 @@ pub fn pat_contains_bindings(dm: &resolve::DefMap, pat: &Pat) -> bool {
     contains_bindings
 }
 
-pub fn simple_identifier<'a>(pat: &'a Pat) -> Option<&'a Ident> {
+pub fn simple_identifier<'a>(pat: &'a ast::Pat) -> Option<&'a ast::Ident> {
     match pat.node {
-        PatIdent(BindByValue(_), ref path1, None) => {
+        ast::PatIdent(ast::BindByValue(_), ref path1, None) => {
             Some(&path1.node)
         }
         _ => {
@@ -127,12 +131,12 @@ pub fn simple_identifier<'a>(pat: &'a Pat) -> Option<&'a Ident> {
     }
 }
 
-pub fn def_to_path(tcx: &ty::ctxt, id: DefId) -> Path {
-    ty::with_path(tcx, id, |mut path| Path {
+pub fn def_to_path(tcx: &ty::ctxt, id: ast::DefId) -> ast::Path {
+    ty::with_path(tcx, id, |mut path| ast::Path {
         global: false,
-        segments: path.last().map(|elem| PathSegment {
-            identifier: Ident::new(elem.name()),
-            parameters: PathParameters::none(),
+        segments: path.last().map(|elem| ast::PathSegment {
+            identifier: ast::Ident::new(elem.name()),
+            parameters: ast::PathParameters::none(),
         }).into_iter().collect(),
         span: DUMMY_SP,
     })