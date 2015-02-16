nikomatsakis/rust

rustc: qualify expressions in check_const for potential promotion.

src/librustc/metadata/common.rs
@@ -142,6 +142,7 @@ pub enum astencode_tag { // Reserves 0x40 -- 0x5f
     tag_table_upvar_borrow_map = 0x55,
     tag_table_capture_modes = 0x56,
     tag_table_object_cast_map = 0x57,
+    tag_table_const_qualif = 0x58,
 }
 
 pub const tag_item_trait_item_sort: uint = 0x60;

flip1995/rust-clippy

src/librustc/metadata/common.rs
@@ -143,6 +143,7 @@ pub enum astencode_tag { // Reserves 0x40 -- 0x5f
     tag_table_upvar_capture_map = 0x56,
     tag_table_capture_modes = 0x57,
     tag_table_object_cast_map = 0x58,
+    tag_table_const_qualif = 0x59,
 }
 
 pub const tag_item_trait_item_sort: uint = 0x60;