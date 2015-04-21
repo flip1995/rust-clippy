rust-lang/rust

Tests for shadowing between lifetimes and loop labels within function bodies.

src/test/compile-fail/loops-reject-duplicate-labels.rs
@@ -1,0 +1,50 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// ignore-tidy-linelength
+
+// Issue #21633: reject duplicate loop labels in function bodies.
+// This is testing the exact cases that are in the issue description.
+
+fn main() {
+    'fl: for _ in 0..10 { break; } //~ NOTE shadowed label `'fl` declared here
+    'fl: loop { break; }           //~ ERROR label name `'fl` shadows a label name that is already in scope
+
+    'lf: loop { break; }           //~ NOTE shadowed label `'lf` declared here
+    'lf: for _ in 0..10 { break; } //~ ERROR label name `'lf` shadows a label name that is already in scope
+
+    'wl: while 2 > 1 { break; }    //~ NOTE shadowed label `'wl` declared here
+    'wl: loop { break; }           //~ ERROR label name `'wl` shadows a label name that is already in scope
+
+    'lw: loop { break; }           //~ NOTE shadowed label `'lw` declared here
+    'lw: while 2 > 1 { break; }    //~ ERROR label name `'lw` shadows a label name that is already in scope
+
+    'fw: for _ in 0..10 { break; } //~ NOTE shadowed label `'fw` declared here
+    'fw: while 2 > 1 { break; }    //~ ERROR label name `'fw` shadows a label name that is already in scope
+
+    'wf: while 2 > 1 { break; }    //~ NOTE shadowed label `'wf` declared here
+    'wf: for _ in 0..10 { break; } //~ ERROR label name `'wf` shadows a label name that is already in scope
+
+    'tl: while let Some(_) = None::<i32> { break; } //~ NOTE shadowed label `'tl` declared here
+    'tl: loop { break; }           //~ ERROR label name `'tl` shadows a label name that is already in scope
+
+    'lt: loop { break; }           //~ NOTE shadowed label `'lt` declared here
+    'lt: while let Some(_) = None::<i32> { break; }
+                                  //~^ ERROR label name `'lt` shadows a label name that is already in scope
+}
+
+// Note however that it is okay for the same label to be reuse in
+// different methods of one impl, as illustrated here.
+
+struct S;
+impl S {
+    fn m1(&self) { 'okay: loop { break 'okay; } }
+    fn m2(&self) { 'okay: loop { break 'okay; } }
+}

src/test/compile-fail/loops-reject-labels-shadowing-lifetimes.rs
@@ -1,0 +1,111 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Issue #21633: reject duplicate loop labels in function bodies.
+// This is testing interaction between lifetime-params and labels.
+
+fn main() {
+    fn foo<'a>() { //~ NOTE shadowed lifetime `'a` declared here
+        'a: loop { break 'a; }
+        //~^ ERROR label name `'a` shadows a lifetime name that is already in scope
+    }
+
+    struct Struct<'b, 'c> { _f: &'b i8, _g: &'c i8 }
+    enum Enum<'d, 'e> { A(&'d i8), B(&'e i8) }
+
+    impl<'d, 'e> Struct<'d, 'e> {
+        fn meth_okay() {
+            'a: loop { break 'a; }
+            'b: loop { break 'b; }
+            'c: loop { break 'c; }
+        }
+    }
+
+    impl <'d, 'e> Enum<'d, 'e> {
+        fn meth_okay() {
+            'a: loop { break 'a; }
+            'b: loop { break 'b; }
+            'c: loop { break 'c; }
+        }
+    }
+
+    impl<'bad, 'c> Struct<'bad, 'c> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad(&self) {
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+
+    impl<'b, 'bad> Struct<'b, 'bad> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad2(&self) {
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+
+    impl<'b, 'c> Struct<'b, 'c> {
+        fn meth_bad3<'bad>(x: &'bad i8) { //~ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+
+        fn meth_bad4<'a,'bad>(x: &'a i8, y: &'bad i8) {
+            //~^ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+
+    impl <'bad, 'e> Enum<'bad, 'e> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad(&self) {
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+    impl <'d, 'bad> Enum<'d, 'bad> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad2(&self) {
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+    impl <'d, 'e> Enum<'d, 'e> {
+        fn meth_bad3<'bad>(x: &'bad i8) { //~ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+
+        fn meth_bad4<'a,'bad>(x: &'bad i8) { //~ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+
+    trait HasDefaultMethod1<'bad> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_okay() {
+            'c: loop { break 'c; }
+        }
+        fn meth_bad(&self) {
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+    trait HasDefaultMethod2<'a,'bad> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad(&self) {
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+    trait HasDefaultMethod3<'a,'b> {
+        fn meth_bad<'bad>(&self) { //~ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ ERROR label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+}

flip1995/rust-clippy

src/test/compile-fail/loops-reject-duplicate-labels.rs
@@ -1,0 +1,60 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![feature(rustc_attrs)]
+
+// ignore-tidy-linelength
+
+// Issue #21633: reject duplicate loop labels in function bodies.
+// This is testing the exact cases that are in the issue description.
+
+fn foo() {
+    'fl: for _ in 0..10 { break; } //~ NOTE shadowed label `'fl` declared here
+    'fl: loop { break; }           //~ WARN label name `'fl` shadows a label name that is already in scope
+
+    'lf: loop { break; }           //~ NOTE shadowed label `'lf` declared here
+    'lf: for _ in 0..10 { break; } //~ WARN label name `'lf` shadows a label name that is already in scope
+
+    'wl: while 2 > 1 { break; }    //~ NOTE shadowed label `'wl` declared here
+    'wl: loop { break; }           //~ WARN label name `'wl` shadows a label name that is already in scope
+
+    'lw: loop { break; }           //~ NOTE shadowed label `'lw` declared here
+    'lw: while 2 > 1 { break; }    //~ WARN label name `'lw` shadows a label name that is already in scope
+
+    'fw: for _ in 0..10 { break; } //~ NOTE shadowed label `'fw` declared here
+    'fw: while 2 > 1 { break; }    //~ WARN label name `'fw` shadows a label name that is already in scope
+
+    'wf: while 2 > 1 { break; }    //~ NOTE shadowed label `'wf` declared here
+    'wf: for _ in 0..10 { break; } //~ WARN label name `'wf` shadows a label name that is already in scope
+
+    'tl: while let Some(_) = None::<i32> { break; } //~ NOTE shadowed label `'tl` declared here
+    'tl: loop { break; }           //~ WARN label name `'tl` shadows a label name that is already in scope
+
+    'lt: loop { break; }           //~ NOTE shadowed label `'lt` declared here
+    'lt: while let Some(_) = None::<i32> { break; }
+                                  //~^ WARN label name `'lt` shadows a label name that is already in scope
+}
+
+// Note however that it is okay for the same label to be reused in
+// different methods of one impl, as illustrated here.
+
+struct S;
+impl S {
+    fn m1(&self) { 'okay: loop { break 'okay; } }
+    fn m2(&self) { 'okay: loop { break 'okay; } }
+}
+
+#[rustc_error]
+pub fn main() { //~ ERROR compilation successful
+    let s = S;
+    s.m1();
+    s.m2();
+    foo();
+}

src/test/compile-fail/loops-reject-labels-shadowing-lifetimes.rs
@@ -1,0 +1,120 @@
+// Copyright 2015 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+// Issue #21633: reject duplicate loop labels in function bodies.
+// This is testing interaction between lifetime-params and labels.
+
+#![feature(rustc_attrs)]
+
+#![allow(dead_code, unused_variables)]
+
+fn foo() {
+    fn foo<'a>() { //~ NOTE shadowed lifetime `'a` declared here
+        'a: loop { break 'a; }
+        //~^ WARN label name `'a` shadows a lifetime name that is already in scope
+    }
+
+    struct Struct<'b, 'c> { _f: &'b i8, _g: &'c i8 }
+    enum Enum<'d, 'e> { A(&'d i8), B(&'e i8) }
+
+    impl<'d, 'e> Struct<'d, 'e> {
+        fn meth_okay() {
+            'a: loop { break 'a; }
+            'b: loop { break 'b; }
+            'c: loop { break 'c; }
+        }
+    }
+
+    impl <'d, 'e> Enum<'d, 'e> {
+        fn meth_okay() {
+            'a: loop { break 'a; }
+            'b: loop { break 'b; }
+            'c: loop { break 'c; }
+        }
+    }
+
+    impl<'bad, 'c> Struct<'bad, 'c> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad(&self) {
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+
+    impl<'b, 'bad> Struct<'b, 'bad> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad2(&self) {
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+
+    impl<'b, 'c> Struct<'b, 'c> {
+        fn meth_bad3<'bad>(x: &'bad i8) { //~ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+
+        fn meth_bad4<'a,'bad>(x: &'a i8, y: &'bad i8) {
+            //~^ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+
+    impl <'bad, 'e> Enum<'bad, 'e> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad(&self) {
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+    impl <'d, 'bad> Enum<'d, 'bad> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad2(&self) {
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+    impl <'d, 'e> Enum<'d, 'e> {
+        fn meth_bad3<'bad>(x: &'bad i8) { //~ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+
+        fn meth_bad4<'a,'bad>(x: &'bad i8) { //~ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+
+    trait HasDefaultMethod1<'bad> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_okay() {
+            'c: loop { break 'c; }
+        }
+        fn meth_bad(&self) {
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+    trait HasDefaultMethod2<'a,'bad> { //~ NOTE shadowed lifetime `'bad` declared here
+        fn meth_bad(&self) {
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+    trait HasDefaultMethod3<'a,'b> {
+        fn meth_bad<'bad>(&self) { //~ NOTE shadowed lifetime `'bad` declared here
+            'bad: loop { break 'bad; }
+            //~^ WARN label name `'bad` shadows a lifetime name that is already in scope
+        }
+    }
+}
+
+#[rustc_error]
+pub fn main() { //~ ERROR compilation successful
+    foo();
+}