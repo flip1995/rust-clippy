rust-lang/rust

Implement type ascription.

src/librustc/middle/check_const.rs
@@ -167,6 +167,7 @@ fn check_expr(v: &mut CheckCrateVisitor, e: &ast::Expr) {
         ast::ExprTupField(..) |
         ast::ExprIndex(..) |
         ast::ExprTup(..) |
+        ast::ExprType(..) |
         ast::ExprRepeat(..) |
         ast::ExprStruct(..) => {}
 

flip1995/rust-clippy

src/librustc/middle/check_const.rs
@@ -784,6 +784,7 @@ fn check_expr<'a, 'tcx>(v: &mut CheckCrateVisitor<'a, 'tcx>,
         hir::ExprField(..) |
         hir::ExprTupField(..) |
         hir::ExprVec(_) |
+        hir::ExprType(..) |
         hir::ExprTup(..) => {}
 
         // Conditional control flow (possible to implement).