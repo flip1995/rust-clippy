rust-lang/rust

Gate tool_attributes feature

src/libsyntax/diagnostic_list.rs
@@ -366,4 +366,5 @@ register_diagnostics! {
     E0589, // invalid `repr(align)` attribute
     E0629, // missing 'feature' (rustc_const_unstable)
     E0630, // rustc_const_unstable attribute must be paired with stable/unstable attribute
+    E0693, // an unknown tool name found in scoped attributes
 }

flip1995/rust-clippy

src/libsyntax/diagnostic_list.rs
@@ -325,4 +325,5 @@ register_diagnostics! {
     E0629, // missing 'feature' (rustc_const_unstable)
     E0630, // rustc_const_unstable attribute must be paired with stable/unstable attribute
     E0693, // incorrect `repr(align)` attribute format
+    E0694, // an unknown tool name found in scoped attributes
 }