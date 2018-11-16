rust-lang/rust

Separating the back folder between backend-agnostic and LLVM-specific code

src/librustc_codegen_ssa/interfaces/mod.rs
@@ -36,6 +36,7 @@ mod intrinsic;
 mod debuginfo;
 mod abi;
 mod asm;
+mod write;
 
 use rustc::ty::Ty;
 use rustc::ty::layout::{LayoutOf, HasTyCtxt, TyLayout};
@@ -52,6 +53,7 @@ pub use self::intrinsic::{IntrinsicCallMethods, IntrinsicDeclarationMethods};
 pub use self::debuginfo::{DebugInfoMethods, DebugInfoBuilderMethods};
 pub use self::abi::{AbiMethods, AbiBuilderMethods};
 pub use self::asm::{AsmMethods, AsmBuilderMethods};
+pub use self::write::{WriteBackendMethods, ThinBufferMethods, ModuleBufferMethods};
 
 
 pub trait CodegenObject : Copy + PartialEq + fmt::Debug {}

flip1995/rust-clippy

src/librustc_codegen_ssa/interfaces/mod.rs
@@ -35,6 +35,7 @@ mod intrinsic;
 mod misc;
 mod statics;
 mod type_;
+mod write;
 
 pub use self::abi::{AbiBuilderMethods, AbiMethods};
 pub use self::asm::{AsmBuilderMethods, AsmMethods};
@@ -49,6 +50,7 @@ pub use self::statics::StaticMethods;
 pub use self::type_::{
     ArgTypeMethods, BaseTypeMethods, DerivedTypeMethods, LayoutTypeMethods, TypeMethods,
 };
+pub use self::write::{ModuleBufferMethods, ThinBufferMethods, WriteBackendMethods};
 
 use std::fmt;
 