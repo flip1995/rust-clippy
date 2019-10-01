rust-lang/rust

address review comments

src/librustc_typeck/check/expr.rs
@@ -148,20 +148,18 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
         debug!(">> type-checking: expr={:?} expected={:?}",
                expr, expected);
 
-        // If when desugaring the try block we ok-wrapped an expression that diverges
-        // (e.g. `try { return }`) then technically the ok-wrapping expression is unreachable.
-        // But since it is autogenerated code the resulting warning is confusing for the user
-        // so we want avoid generating it.
-        // Ditto for the autogenerated `Try::from_ok(())` at the end of e.g. `try { return; }`.
-        let (is_try_block_ok_wrapped_expr, is_try_block_generated_expr) = match expr.node {
-            ExprKind::Call(_, ref args) if expr.span.is_desugaring(DesugaringKind::TryBlock) => {
-                (true, args.len() == 1 && args[0].span.is_desugaring(DesugaringKind::TryBlock))
-            }
-            _ => (false, false),
+        // True if `expr` is a `Try::from_ok(())` that is a result of desugaring a try block
+        // without the final expr (e.g. `try { return; }`). We don't want to generate an
+        // unreachable_code lint for it since warnings for autogenerated code are confusing.
+        let is_try_block_generated_unit_expr = match expr.node {
+            ExprKind::Call(_, ref args) if expr.span.is_desugaring(DesugaringKind::TryBlock) =>
+                args.len() == 1 && args[0].span.is_desugaring(DesugaringKind::TryBlock),
+
+            _ => false,
         };
 
         // Warn for expressions after diverging siblings.
-        if !is_try_block_generated_expr {
+        if !is_try_block_generated_unit_expr {
             self.warn_if_unreachable(expr.hir_id, expr.span, "expression");
         }
 
@@ -174,15 +172,17 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
         let ty = self.check_expr_kind(expr, expected, needs);
 
         // Warn for non-block expressions with diverging children.
-        if !is_try_block_ok_wrapped_expr {
-            match expr.node {
-                ExprKind::Block(..) | ExprKind::Loop(..) | ExprKind::Match(..) => {},
-                ExprKind::Call(ref callee, _) =>
-                    self.warn_if_unreachable(expr.hir_id, callee.span, "call"),
-                ExprKind::MethodCall(_, ref span, _) =>
-                    self.warn_if_unreachable(expr.hir_id, *span, "call"),
-                _ => self.warn_if_unreachable(expr.hir_id, expr.span, "expression"),
-            }
+        match expr.node {
+            ExprKind::Block(..) | ExprKind::Loop(..) | ExprKind::Match(..) => {},
+            // If `expr` is a result of desugaring the try block and is an ok-wrapped
+            // diverging expression (e.g. it arose from desugaring of `try { return }`),
+            // we skip issuing a warning because it is autogenerated code.
+            ExprKind::Call(..) if expr.span.is_desugaring(DesugaringKind::TryBlock) => {},
+            ExprKind::Call(ref callee, _) =>
+                self.warn_if_unreachable(expr.hir_id, callee.span, "call"),
+            ExprKind::MethodCall(_, ref span, _) =>
+                self.warn_if_unreachable(expr.hir_id, *span, "call"),
+            _ => self.warn_if_unreachable(expr.hir_id, expr.span, "expression"),
         }
 
         // Any expression that produces a value of type `!` must have diverged

flip1995/rust-clippy

src/librustc_typeck/check/expr.rs
@@ -151,20 +151,18 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
         debug!(">> type-checking: expr={:?} expected={:?}",
                expr, expected);
 
-        // If when desugaring the try block we ok-wrapped an expression that diverges
-        // (e.g. `try { return }`) then technically the ok-wrapping expression is unreachable.
-        // But since it is autogenerated code the resulting warning is confusing for the user
-        // so we want avoid generating it.
-        // Ditto for the autogenerated `Try::from_ok(())` at the end of e.g. `try { return; }`.
-        let (is_try_block_ok_wrapped_expr, is_try_block_generated_expr) = match expr.node {
-            ExprKind::Call(_, ref args) if expr.span.is_desugaring(DesugaringKind::TryBlock) => {
-                (true, args.len() == 1 && args[0].span.is_desugaring(DesugaringKind::TryBlock))
-            }
-            _ => (false, false),
+        // True if `expr` is a `Try::from_ok(())` that is a result of desugaring a try block
+        // without the final expr (e.g. `try { return; }`). We don't want to generate an
+        // unreachable_code lint for it since warnings for autogenerated code are confusing.
+        let is_try_block_generated_unit_expr = match expr.node {
+            ExprKind::Call(_, ref args) if expr.span.is_desugaring(DesugaringKind::TryBlock) =>
+                args.len() == 1 && args[0].span.is_desugaring(DesugaringKind::TryBlock),
+
+            _ => false,
         };
 
         // Warn for expressions after diverging siblings.
-        if !is_try_block_generated_expr {
+        if !is_try_block_generated_unit_expr {
             self.warn_if_unreachable(expr.hir_id, expr.span, "expression");
         }
 
@@ -177,15 +175,17 @@ impl<'a, 'tcx> FnCtxt<'a, 'tcx> {
         let ty = self.check_expr_kind(expr, expected, needs);
 
         // Warn for non-block expressions with diverging children.
-        if !is_try_block_ok_wrapped_expr {
-            match expr.kind {
-                ExprKind::Block(..) | ExprKind::Loop(..) | ExprKind::Match(..) => {},
-                ExprKind::Call(ref callee, _) =>
-                    self.warn_if_unreachable(expr.hir_id, callee.span, "call"),
-                ExprKind::MethodCall(_, ref span, _) =>
-                    self.warn_if_unreachable(expr.hir_id, *span, "call"),
-                _ => self.warn_if_unreachable(expr.hir_id, expr.span, "expression"),
-            }
+        match expr.kind {
+            ExprKind::Block(..) | ExprKind::Loop(..) | ExprKind::Match(..) => {},
+            // If `expr` is a result of desugaring the try block and is an ok-wrapped
+            // diverging expression (e.g. it arose from desugaring of `try { return }`),
+            // we skip issuing a warning because it is autogenerated code.
+            ExprKind::Call(..) if expr.span.is_desugaring(DesugaringKind::TryBlock) => {},
+            ExprKind::Call(ref callee, _) =>
+                self.warn_if_unreachable(expr.hir_id, callee.span, "call"),
+            ExprKind::MethodCall(_, ref span, _) =>
+                self.warn_if_unreachable(expr.hir_id, *span, "call"),
+            _ => self.warn_if_unreachable(expr.hir_id, expr.span, "expression"),
         }
 
         // Any expression that produces a value of type `!` must have diverged