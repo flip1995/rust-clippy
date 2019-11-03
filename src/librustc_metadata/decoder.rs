fclemonschool/rust

rustc_metadata: replace predicates_defined_on with explicit_predicates_of and inferred_outlives_of.

src/librustc_metadata/decoder.rs
@@ -617,11 +617,20 @@ impl<'a, 'tcx> CrateMetadata {
         tcx.alloc_adt_def(did, kind, variants, repr)
     }
 
-    pub fn get_predicates_defined_on(&self,
-                                   item_id: DefIndex,
-                                   tcx: TyCtxt<'a, 'tcx, 'tcx>)
-                                   -> ty::GenericPredicates<'tcx> {
-        self.entry(item_id).predicates_defined_on.unwrap().decode((self, tcx))
+    pub fn get_explicit_predicates(
+        &self,
+        item_id: DefIndex,
+        tcx: TyCtxt<'a, 'tcx, 'tcx>,
+    ) -> ty::GenericPredicates<'tcx> {
+        self.entry(item_id).explicit_predicates.unwrap().decode((self, tcx))
+    }
+
+    pub fn get_inferred_outlives(
+        &self,
+        item_id: DefIndex,
+        tcx: TyCtxt<'a, 'tcx, 'tcx>,
+    ) -> Vec<ty::Predicate<'tcx>> {
+        self.entry(item_id).inferred_outlives.unwrap().decode((self, tcx))
     }
 
     pub fn get_super_predicates(&self,

flip1995/rust-clippy

src/librustc_metadata/decoder.rs
@@ -658,12 +658,20 @@ impl<'a, 'tcx> CrateMetadata {
         tcx.alloc_adt_def(did, adt_kind, variants, repr)
     }
 
-    crate fn get_predicates_defined_on(
+    crate fn get_explicit_predicates(
         &self,
         item_id: DefIndex,
         tcx: TyCtxt<'tcx>,
     ) -> ty::GenericPredicates<'tcx> {
-        self.root.per_def.predicates_defined_on.get(self, item_id).unwrap().decode((self, tcx))
+        self.root.per_def.explicit_predicates.get(self, item_id).unwrap().decode((self, tcx))
+    }
+
+    crate fn get_inferred_outlives(
+        &self,
+        item_id: DefIndex,
+        tcx: TyCtxt<'tcx>,
+    ) -> &'tcx [(ty::Predicate<'tcx>, Span)] {
+        self.root.per_def.inferred_outlives.get(self, item_id).unwrap().decode((self, tcx))
     }
 
     crate fn get_super_predicates(