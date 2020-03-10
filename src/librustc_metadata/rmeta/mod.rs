anyska/rust

Store `TokenStream` in `rmeta::MacroDef`. This removes a hack from `load_macro_untracked` in which parsing is used.

src/librustc_metadata/rmeta/mod.rs
@@ -23,6 +23,7 @@ use rustc_span::symbol::Symbol;
 use rustc_span::{self, Span};
 use rustc_target::spec::{PanicStrategy, TargetTriple};
 use syntax::ast;
+use syntax::tokenstream::TokenStream;
 
 use std::marker::PhantomData;
 use std::num::NonZeroUsize;
@@ -324,7 +325,7 @@ struct ModData {
 
 #[derive(RustcEncodable, RustcDecodable)]
 struct MacroDef {
-    body: String,
+    body: TokenStream,
     legacy: bool,
 }
 

flip1995/rust-clippy

src/librustc_metadata/rmeta/mod.rs
@@ -11,6 +11,7 @@ use rustc::session::config::SymbolManglingVersion;
 use rustc::session::CrateDisambiguator;
 use rustc::ty::{self, ReprOptions, Ty};
 use rustc_ast::ast;
+use rustc_ast::tokenstream::TokenStream;
 use rustc_attr as attr;
 use rustc_data_structures::svh::Svh;
 use rustc_data_structures::sync::MetadataRef;
@@ -324,7 +325,7 @@ struct ModData {
 
 #[derive(RustcEncodable, RustcDecodable)]
 struct MacroDef {
-    body: String,
+    body: TokenStream,
     legacy: bool,
 }
 