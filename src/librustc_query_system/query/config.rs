jeetshah47/rust

Monomorphise try_execute_anon_query.

src/librustc/ty/query/config.rs
@@ -27,6 +27,8 @@ pub trait QueryConfig<'tcx> {
 }
 
 pub(crate) struct QueryVtable<'tcx, K, V> {
+    pub anon: bool,
+    pub dep_kind: DepKind,
     pub eval_always: bool,
 
     // Don't use this method to compute query results, instead use the methods on TyCtxt
@@ -98,6 +100,8 @@ pub(crate) trait QueryDescription<'tcx>: QueryAccessors<'tcx> {
 
     fn reify() -> QueryVtable<'tcx, Self::Key, Self::Value> {
         QueryVtable {
+            anon: Self::ANON,
+            dep_kind: Self::DEP_KIND,
             eval_always: Self::EVAL_ALWAYS,
             compute: Self::compute,
             hash_result: Self::hash_result,

flip1995/rust-clippy

src/librustc_query_system/query/config.rs
@@ -25,6 +25,8 @@ pub trait QueryConfig<CTX> {
 }
 
 pub(crate) struct QueryVtable<CTX: QueryContext, K, V> {
+    pub anon: bool,
+    pub dep_kind: CTX::DepKind,
     pub eval_always: bool,
 
     // Don't use this method to compute query results, instead use the methods on TyCtxt
@@ -103,6 +105,8 @@ where
     Q: QueryDescription<CTX>,
 {
     const VTABLE: QueryVtable<CTX, Q::Key, Q::Value> = QueryVtable {
+        anon: Q::ANON,
+        dep_kind: Q::DEP_KIND,
         eval_always: Q::EVAL_ALWAYS,
         compute: Q::compute,
         hash_result: Q::hash_result,